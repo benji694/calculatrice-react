bf5f42b9e0689ea5e6025b96a56a7ffae62d07d5
benji694 <bbouillard@edenschool.fr>
2022-04-05 11:51:44 +0200

    display first number on the screen of calculor (bug)

--- src/component/App.js
+++ src/component/App.js
@@ -1,21 +1,33 @@
-import './App.css';
-import React from 'react';
-import ButtonPannel from './ButtonPannel.js'
-import Result from './Result.js'
+import "./App.css";
+import React from "react";
+import ButtonPannel from "./ButtonPannel.js";
+import Result from "./Result.js";
+import { useState } from "react";
 
 function App() {
-  
-  let nb1 = 0;
-  let calcul = '';
+  let [nb1, setNb1] = useState(0);
+  let calcul = "";
   let nb2;
+  let [resultat, setResultat] = useState(0);
 
   const clickButton = (txt) => {
-    console.log(txt)
-  }
+    if (calcul === "") {
+      if (Number.isInteger(txt)) {
+        if (nb1 === 0) {
+          setNb1(txt);
+        }else {
+          setNb1(parseInt(nb1 + txt.toString()));
+        }
+      }
+      setResultat(nb1);
+      console.log(nb1);
+    }
+  };
+
   return (
     <div className="App">
-      <Result/>
-      <ButtonPannel clickButton={clickButton}/>
+      <Result result={resultat} />
+      <ButtonPannel clickButton={clickButton} />
     </div>
   );
 }
--- src/component/Result.js
+++ src/component/Result.js
@@ -2,10 +2,10 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import "./Result.css";
 
-function Result() {
+function Result(props) {
   return (
     <div className="result">
-      
+      <h1>{props.result}</h1>
     </div>
   );
 }
